031cb7ef4c37860e56a96930d29a0e0098c166af
芸豆玥 <85783168+YunDouYue@users.noreply.github.com>
2023-10-13 18:30:26 +0800

    Update dialogue.js

--- dialogue.js
+++ dialogue.js
@@ -1,4 +1,4 @@
-var botui = new BotUI('dialogue');
+var botui = new BotUI('hello-world');
 
     botui.message.bot({
         delay: 200,
@@ -44,12 +44,12 @@ var botui = new BotUI('dialogue');
         }).then(function() {
             return botui.message.bot({
                 delay: 1500,
-                content: "15岁，是初二"
+                content: "16岁，是职一"
             })
         }).then(function() {
             return botui.message.bot({
                 delay: 1500,
-                content: "略懂 HTML/PHP/JAVA，有空也会研究SSH"
+                content: "略懂 HTML/PHP/JAVA，喜欢学习计算机内容"
             })
         }).then(function() {
             return botui.message.bot({
@@ -59,26 +59,49 @@ var botui = new BotUI('dialogue');
         }).then(function() {
             return botui.message.bot({
                 delay: 1500,
-                content: "喜欢折腾，热爱折腾，目前正在计算机一道上探索中"
+                content: "喜欢折腾，热爱折腾，目前正在电学一道上探索中"
             })
         }).then(function() {
             return botui.action.button({
                 delay: 1500,
                 action: [{
-                    text: "为什么叫 芸豆玥-汤包 呢？ 🤔",
+                    text: "你目前在学习什么？ 🤔",
                     value: "next"
                 }]
             })
         }).then(function(res) {
             return botui.message.bot({
                 delay: 1500,
-                content: "芸豆玥是一次起BOT名字时偶然打出了这个名字，于是我就给自己用了！"
+                content: "在填写志愿时对比了所有的专业难易、类型而选择了机电！"
             })
         }).then(function() {
             return botui.action.button({
                 delay: 1500,
                 action: [{
-                    text: "你最喜欢的事情是啥？",
+                    text: "为什么偏偏是机电？🤔",
+                    value: "next"
+                }]
+            })
+        }).then(function(res) {
+            return botui.message.bot({
+                delay: 1500,
+                content: "嘻嘻，我喜欢困难的内容（可能心理有点bt？），既要学习机械，还要学习电学！"
+            })
+        }).then(function(res) {
+            return botui.message.bot({
+                delay: 1500,
+                content: "难度指数在所有专业中难度系数最高，同时也是高考录取分数线最低的一个专业"
+            })
+        }).then(function(res) {
+            return botui.message.bot({
+                delay: 1500,
+                content: "目前往本科高校持续前进，希望高考530就能本科保底啦~"
+            })
+        }).then(function() {
+            return botui.action.button({
+                delay: 1500,
+                action: [{
+                    text: "你还喜欢什么呢？🤔",
                     value: "next"
                 }]
             })
